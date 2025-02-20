aacf871abf24c561e42ae1e8e7d9f0f6a5400516
Jamis <jamessamson322@gmail.com>
2025-02-20 12:03:44 +0800

    First commit

--- Collections/Vector/Vector.h
+++ Collections/Vector/Vector.h
@@ -9,25 +9,26 @@ typedef struct iter VecIterator;
 
 #define self Vec*vec
 
-#define stackmem_vec(memory_size, capacity) \
-    {\
-        .ptr = (char[capacity * memory_size]){0},\
-        .cap = capacity,\
-        .top = 0,\
-        .memSize = memory_size\
-    }
+Vec new_vector(size_t memSize, size_t capacity);
 
-void new_vector(size_t memSize, size_t capacity);
-
-bool vector_push(self, void* ptr);
-bool vector_pop(self, void* returnVal);
+bool vector_push(self, const void*);
+bool vector_pop_cpy(self, void* dest);
+void* vector_pop(self);
 
 void* vector_peek(self);
-bool vector_empty(self);
+bool vector_is_empty(self);
+void* vector_get_index(self, size_t index);
+
+void vector_free(self);
 
 
+#undef self
 
+#define self VecIterator*iter
 VecIterator vector_iter(self);
+
+void* vec_iter_next(self);
+
 
 #undef self
 #endif
